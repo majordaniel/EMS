9a217f2f617f8397138bf91a891b1e2e2d7006ea
touhidulfahim <muhdtohidul@gmail.com>
2018-01-09 22:19:19 +0600

    add employeeEducation Model

---
+++ EMS/Models/EmployeeEducation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EMS.Models
+{
+    public class EmployeeEducation
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please select employee reg. no.!")]
+        [Display(Name = "Employee Reg.No.")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please select education!")]
+        [Display(Name = "Education")]
+        public int EducationId { get; set; }
+
+        [Required(ErrorMessage = "Please select exam!")]
+        [Display(Name = "Exam")]
+        public int ExamId { get; set; }
+
+        [Required(ErrorMessage = "Please input group name!")]
+        [Display(Name = "Major Group")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please input institute name!")]
+        [Display(Name = "Institute")]
+        public string InstituteName { get; set; }
+
+        [Required(ErrorMessage = "Please set from date!")]
+        [Display(Name = "From")]
+        public DateTime FromDate { get; set; }
+
+        [Required(ErrorMessage = "Please set to date!")]
+        [Display(Name = "To")]
+        public DateTime ToDate { get; set; }
+
+        [Required(ErrorMessage = "Please input duration")]
+        [StringLength(10)]
+        public string Duration { get; set; }
+
+        [Required(ErrorMessage = "Please set passing year")]
+        [Display(Name = "Passing year")]
+        public int PassingYear { get; set; }
+
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+
+        [ForeignKey("EducationId")]
+        public virtual Education Education { get; set; }
+
+        [ForeignKey("ExamId")]
+        public virtual Exam Exam { get; set; }
+
+    }
+}
