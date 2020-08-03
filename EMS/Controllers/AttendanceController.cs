252291a30e8a0d948440f379cd2898290fb614fc
TOUHID <muhdtohidul@gmail.com>
2020-08-03 23:37:07 +0600

    AttendanceControlle Re-Added

---
+++ EMS/Controllers/AttendanceController.cs
@@ -0,0 +1,43 @@
+﻿using EMS.Gateway;
+using EMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMS.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        private EMSDbContext db = new EMSDbContext();
+
+        public ActionResult ViewAttendance()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SaveAttendance()
+        {
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "EmployeeRegNo");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveAttendance(Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Attendances.Add(attendance);
+                db.SaveChanges();
+                return RedirectToAction("ViewAttendance");
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "EmployeeRegNo", attendance.EmployeeId);
+            return View();
+        }
+
+
+    }
+}
