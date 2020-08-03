6d33d13f12e8565bf61ec5d7a05c828ac9fe2857
TOUHID <muhdtohidul@gmail.com>
2020-08-03 23:25:01 +0600

    Fixed AppUser Could Not Found....

---
+++ EMS/Models/User/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EMS.Models.User
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        [ForeignKey("RoleId")]
+        public virtual Role Roles { get; set; }
+
+    }
+}
